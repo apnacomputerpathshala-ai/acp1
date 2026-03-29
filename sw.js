880cfb99dd1230be6ea57a2c050cf3241cfebcc7
apnacomputerpathshala-ai <apnacomputerpathshala@gmail.com>
2026-03-29 20:55:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,10 @@ const CACHE_NAME = 'acp-portal-v1';
 const assets = [
   './index.html',
   './manifest.json',
-  './logo-.png'
+  './logo-.jpg'
 ];
 
+// 1. Install Event: फाइलों को कैश में सेव करना
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,6 +14,20 @@ self.addEventListener('install', (e) => {
   );
 });
 
+// 2. Activate Event: पुराने और बेकार कैश को डिलीट करना (यहाँ जोड़ा गया है)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// 3. Fetch Event: ऑफलाइन सपोर्ट के लिए
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
